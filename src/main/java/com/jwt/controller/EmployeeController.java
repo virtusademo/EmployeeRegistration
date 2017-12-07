08482a23b7c27b85b3dab049b8ccc3cd54eb1285
virtusademo <virtusademo@gmail.com>
2017-12-07 19:39:14 +0530

    Update EmployeeController.java

--- src/main/java/com/jwt/controller/EmployeeController.java
+++ src/main/java/com/jwt/controller/EmployeeController.java
@@ -74,7 +74,7 @@ public class EmployeeController {
 	public ModelAndView deleteEmployee(HttpServletRequest request) {
 		int employeeId = Integer.parseInt(request.getParameter("id"));
 		employeeService.deleteEmployee(employeeId);
-		return new ModelAndView("result");
+		return new ModelAndView("home");
 	}
 
 	@RequestMapping(value = "/editEmployee", method = RequestMethod.GET)
